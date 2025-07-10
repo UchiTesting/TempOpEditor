975cb149684140ccbbf288ccd1d8b82c487459d8
UchiTesting <uchitesting57@gmail.com>
2025-07-10 23:40:06 +0200

    Add ToQifFormat method to Operation model

--- src/TempOpBll/Models/Operation.cs
+++ src/TempOpBll/Models/Operation.cs
@@ -1,18 +1,43 @@
-﻿namespace TempOpBll.Models
+﻿using System.Globalization;
+
+namespace TempOpBll.Models
 {
     public class Operation
     {
-        public string Label { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty;
+        #region Properties
         public decimal Amount { get; set; }
+        public string Category { get; set; } = string.Empty;
         public string Comment { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Label { get; set; } = string.Empty;
         public int? Quantity { get; set; } = null;
+        #endregion
 
+        /// <summary>
+        /// Outputs a string representation of the operation
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             var qtyText = Quantity.HasValue ? $"{Quantity.Value}× " : "";
             var commentText = string.IsNullOrWhiteSpace(Comment) ? "" : $" ({Comment})";
             return $"{Category} {qtyText}{Label}{commentText} {Amount:0.00}€";
+        }
+
+        /// <summary>
+        /// Output operation details to QIF format
+        /// </summary>
+        /// <returns></returns>
+        public string ToQifString()
+        {
+            string date = Date.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+            string amount = Amount.ToString("-0.00", CultureInfo.InvariantCulture);
+            string label = Quantity.HasValue ? $"{Label} x{Quantity}" : Label;
+            string memo = !string.IsNullOrWhiteSpace(Comment)
+                ? $"{Category} - {Comment}"
+                : Category;
+
+            return $"D{date}{Environment.NewLine}T{amount}{Environment.NewLine}P{label}{Environment.NewLine}M{memo}{Environment.NewLine}^";
         }
     }
 }
