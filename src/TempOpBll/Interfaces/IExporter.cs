67152cd7ab594f2132db42f816a268c7f50ef8b3
UchiTesting <uchitesting57@gmail.com>
2025-07-13 15:30:29 +0200

    Add IExporter interface.
    Instead of a class method we are going to use a dedicated class to export data.

---
+++ src/TempOpBll/Interfaces/IExporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempOpBll.Interfaces
+{
+    public interface IExporter<T>
+    {
+        /// <summary>
+        /// Exports the specified input to a string representation.
+        /// </summary>
+        /// <param name="input">The input object to be exported. Cannot be null.</param>
+        /// <returns>A string representation of the input object.</returns>
+        string Export(T input);
+    }
+}
